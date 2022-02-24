bc18d585bf7d0d443c76baeb3c291500ed790474
pavo-etc <z1verdeman2@gmail.com>
2022-02-24 14:19:13 +0800

    Add last update information to site

--- html_generator.py
+++ html_generator.py
@@ -1,5 +1,6 @@
 import jinja2
 import os
+from datetime import datetime, timezone, timedelta
 
 def generate_index():
     os.system("sqlite3 wa_covid_data.db -html -header \"SELECT * FROM daily_cases ORDER BY date DESC\" > site/table.html")
@@ -7,10 +8,14 @@ def generate_index():
     with open("site/table.html") as f:
         table = f.read()
 
+    now_utc = datetime.now()
+    local_tz = timezone(timedelta(hours=8))
+    current_time_awst = now_utc.astimezone(local_tz).strftime("%Y-%m-%d %H:%M:%S") + " AWST (UTC+8)"
+
     outputfile = "./site/index.html"
     subs = jinja2.Environment( 
                 loader=jinja2.FileSystemLoader('./site/')      
-                ).get_template('template.html').render(table=table) 
+                ).get_template('template.html').render(table=table, update_time=current_time_awst) 
 
-    with open(outputfile,'w') as f:
+    with open(outputfile,'w', encoding="utf-8") as f:
         f.write(subs)
