041d34ece3bd2c52b281535028a94706e23792d5
pavo-etc <z1verdeman2@gmail.com>
2022-02-27 02:54:56 +0800

    I hate this

--- plot_generator.py
+++ plot_generator.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def generate_local_cases(data, n_days):
+
+def generate_local_cases(data, n_days, dark=False):
+    plt.style.use("default")
+
+    if dark:
+        #plt.style.use("./site/styles/dracula.mplstyle")
+        plt.style.use("dark_background")
+
+
+
     plt.xticks(rotation="vertical", fontsize="xx-small")
 
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
@@ -16,12 +25,20 @@ def generate_local_cases(data, n_days):
 
     plt.plot(data["dates"][-n_days:],data["local"][-n_days:])
 
-    plt.rcParams['savefig.dpi'] = 300
+    #lt.rcParams['savefig.dpi'] = 300
     plt.tight_layout()
 
-    filename = "./site/images/local_cases.png"
-    plt.savefig(filename)
-    print(f"Generated {filename} with {n_days} days.")
+    folder = "./site/images"
+    if dark: 
+        filename = f"{folder}/local_cases_dark.png"
+    else: 
+        filename = f"{folder}/local_cases.png"
     
+    plt.savefig(filename, dpi=300, transparent=True)#, facecolor="#282A36")
+
+    print(f"Generated {filename} with {n_days} days.")
+
 def generate_all_plots(data, n_days):
-    generate_local_cases(data, n_days)
+    generate_local_cases(data, n_days)    
+    # apparently plt.styles.use() REQUIRES a literal. Don't know why
+    generate_local_cases(data, n_days, dark=True)
